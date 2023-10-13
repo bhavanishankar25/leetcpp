6f4bf1b77373f22c272525bd14af8ee674e8a88e
bhavanishankar25 <47072061+bhavanishankar25@users.noreply.github.com>
2023-10-13 16:22:21 +0530

    Time: 69 ms (56.18%) | Memory: 91.2 MB (45.18%) - LeetSync

--- 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.cpp
+++ 2866-longest-even-odd-subarray-with-threshold/longest-even-odd-subarray-with-threshold.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int longestAlternatingSubarray(vector<int>& nums, int threshold) {
-        int n = nums.size();
-        int left = 0;   
-        int right = 0; 
-        int res = 0;
-        
-        while (right < n) {
-            if (right > 0 && (nums[right] % 2 == nums[right - 1] % 2 || nums[right] > threshold)) {
-                left = right;
+        int n=nums.size();
+        int left=0;
+        int right=0;
+        int res=0;
+        while(right <n){
+            if(right>0 && (nums[right]%2==nums[right-1]%2 || nums[right]>threshold)){
+                left=right;
             }
-            if(nums[left]%2!=0||nums[left]>threshold) left++;
-            res = max(res, right - left + 1);
-            
+            if(nums[left]%2!=0 ||  nums[left]>threshold ){
+                left++;
+            }
+            res=max(res , right-left+1);
             right++;
+            
         }
-        
         return res;
     }
 };
